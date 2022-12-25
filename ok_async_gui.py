88bb81ba07abda76d71610ef9042a03be2e1043c
ea7kir <mikenaylorspain@icloud.com>
2022-12-25 19:32:07 +0100

    tidyup

--- ok_async_gui.py
+++ ok_async_gui.py
@@ -1,47 +1,49 @@
 import PySimpleGUI as sg
 import asyncio
-import random
+from random import randint
 import sys
 
 sg.change_look_and_feel('DarkAmber')
 
 layout = [
     [sg.Text('')],
-    [sg.Text('Password:'), sg.InputText(password_char='*', key='password')],
-    [sg.Text('', key='status', size=(20, 1))],
-    [sg.Button('Submit'), sg.Button('Çancel')],
+    [sg.Text('Enter text:'), sg.InputText(key='-Text-')],
+    [sg.Text('', key='-Status-', size=(20, 1))],
+    [sg.Button('Submit', key='-Submit-'), sg.Button('Çancel', key='-Cancel-')],
 ]
 
 window = sg.Window('Dripbox', layout, finalize=True)
 
+running = True
+
 async def background():
-    while True:
-        rando = random.randint(2, 2000)
+    global running
+    while running:
+        rando = randint(2, 2000)
         print(rando)
-        window['status'].update(rando)
+        window['-Status-'].update(rando)
         await asyncio.sleep(1)
 
 async def ui():
-    while True:
+    global running
+    while running:
         event, value = window.read(timeout=1)
-        if event in (None, 'Çancel'):
-            sys.exit()
+        if event == sg.WIN_CLOSED or event == '-Cancel-':
+            running = False
         if event != '__TIMEOUT__':
             print(f"{event} - {value}")
         await asyncio.sleep(0)
-
-#async def wait_list():
-#    await asyncio.wait([ui(), background()]) # I changed the order, but make no dif
 
 async def main():
     await asyncio.gather(
         ui(),
         background(),
     )
+    print('All tasks stoped')
+    window.close()
+    print('window closed')
 
 if __name__ == '__main__':
-#    loop = asyncio.get_event_loop()
-#    loop.run_until_complete(wait_list())
-#    loop.close()
     asyncio.run(main())
+    print('end')
     
